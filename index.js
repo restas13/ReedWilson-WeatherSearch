56907f4caec36c2d590aaf2f72a5eb970a25f0b5
restas13 <72560360+restas13@users.noreply.github.com>
2023-08-01 15:13:29 -0700

    adding the function to render the current weather and adding an event listener for future testning as well as a placeholder 'heandleSearvhZSubmit' for the event listener

--- index.js
+++ index.js
@@ -10,4 +10,55 @@ let currentContainer = document.querySelector('#today');
 let forcastCNont = document.querySelector('#forecast');
 let historyContnainer = document.querySelector('#search-history');
 
+// Time plugins
+dayjs.extend(window.dayjs_plugin_utc);
+dayjs.extend(window.dayjs_plugin_timezone);
 
+// Renders the current weather 
+function renderWeather(city, weather) {
+    // 
+    let temperature = weather.main.temp;
+    let winds = weather.wind.speed;
+    let humidity = weather.main.humidity;
+    let iconUrl = `https://openweathermap.org/img/w/${weather.weather[0].icon}.png`;
+    let description = weather.weather[0].description || weather[0].main;
+    let date = dayjs().format('M/D/YYYY');
+
+    let card = document.createElement('div');
+    let body = document.createElement('div');
+    let header = document.createElement('h2');
+    let weatherIcon = document.createElement('img');
+    let temp = document.createElement('p');
+    let wind = document.createElement('p');
+    let humidityElement = document.createElement('p');
+
+    card.setAttribute('class', 'card');
+    body.setAttribute('class', 'card-body');
+    card.append(body);
+
+    header.setAttribute('class', 'h3 card-title');
+    temp.setAttribute('class', 'card-text');
+    wind.setAttribute('class', 'card-text');
+    humidityElement.setAttribute('class', 'card-text');
+
+    header.textContent = `${city} (${date})`;
+    weatherIcon.setAttribute('src', iconUrl);
+    weatherIcon.setAttribute('alt', description);
+    weatherIcon.setAttribute('class', 'weather-img');
+    header.append(weatherIcon);
+    temp.textContent = `Temp: ${temperature}°F`;
+    wind.textContent = `Wind: ${winds} MPH`;
+    humidityEl.textContent = `Humidity is at ${humidity}%`;
+    body.append(header, temp, wind, humidityElement);
+
+    currentContainer.innerHTML = '';
+    currentContainer.append(card);
+}
+
+// Function for when the user submits the city
+function handleSearchSubmit() {
+
+}
+
+// Listening for the submit button or the enter key
+searchDiv.addEventListener('submit', handleSearchSubmit);
