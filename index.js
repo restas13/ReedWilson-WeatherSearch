23ba74d321913064db1d121fe501b0041647300b
restas13 <72560360+restas13@users.noreply.github.com>
2023-08-01 14:36:23 -0700

    adding the DOM references to the index.js variables as well as the api link and key

---
+++ index.js
@@ -0,0 +1,13 @@
+// Setting the constant api variables && search history variable
+let history = [];
+let apiKey = `a145f8e44dde584518f485491c3469d3`;
+let apiUrl = `https://api.openweathermap.org`;
+
+// Getting the html elements
+let searchDiv = document.querySelector('#search-div');
+let searchBar = document.querySelector('#search-bar');
+let currentContainer = document.querySelector('#today');
+let forcastCNont = document.querySelector('#forecast');
+let historyContnainer = document.querySelector('#search-history');
+
+
