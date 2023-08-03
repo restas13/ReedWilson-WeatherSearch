be71bf299398e65410a0482ce6201b9a6652714b
restas13 <72560360+restas13@users.noreply.github.com>
2023-08-03 15:42:38 -0700

    Added the current weather functionality, and fixxed the bugs with returning the fetch data. The app now accepts the city, and returns the temperature, wind, and humidity for the day

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ function fetchLocation(search) {
 
     fetch(url).then((response) => {
         return response.json();
-    }).then((data)  => {
-        if(data[0]) {
+    }).then((data) => {
+        if (data[0]) {
             console.log(data[0]);
             getWeather(data[0]);
         }
@@ -28,16 +28,67 @@ function fetchLocation(search) {
 }
 
 function getWeather(location) {
-    const {lon} = location;
-    const {lat} = location;
+    const { lon } = location;
+    const { lat } = location;
+
+    const city = location.name;
 
     fetch(`${apiRoot}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`)
-    .then((response) => {
-        return response.json();
-    }).then((data) => {
-
-    }).catch((error) => {
-        console.log(error);
-    });
+        .then((response) => {
+            console.log(response);
+            return response.json();
+        }).then((data) => {
+            renderToday(city, data.list[0])
+        }).catch((error) => {
+            console.log(error);
+        });
 
 }
+
+function renderToday(city, weather) {
+    console.log(weather);
+    const temperature = weather.main.temp;
+    const winds = weather.wind.speed;
+    const humid = weather.main.humidity;
+
+    createCard(city, temperature, winds, humid, 'today');
+
+}
+
+function createCard(city, temperature, winds, humid, type) {
+    let card = document.createElement('div');
+    let cardBody = document.createElement('div');
+    let header = document.createElement('h2');
+    let temp = document.createElement('p');
+    let wind = document.createElement('p');
+    let humidity = document.createElement('p');
+
+    if (type == 'today') {
+        console.log('todays forecast');
+
+        header.textContent = city;
+        temp.textContent = `Temperature: ${temperature}`;
+        wind.textContent = `Wind: ${winds}`;
+        humidity.textContent = `Humidity: ${humid}`;
+        cardBody.append(temp, wind, humidity);
+
+        card.append(header, cardBody);
+
+        todaysWeather.innerHTML = '';
+        todaysWeather.append(card);
+    }
+}
+
+function handleSubmit(event) {
+    if (!input.value) {
+        console.log('bug');
+        return;
+    }
+
+    event.preventDefault();
+    let search = input.value.trim();
+    fetchLocation(search);
+}
+
+
+document.querySelector('#Submit-btn').addEventListener('click', handleSubmit);
