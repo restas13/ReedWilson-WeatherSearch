69fd5a463a72a4e0aafb9266d88b9f0754ff6294
restas13 <72560360+restas13@users.noreply.github.com>
2023-08-03 15:04:07 -0700

    Fetch location function added

--- index.js
+++ index.js
@@ -11,3 +11,22 @@ let apiKey = 'a145f8e44dde584518f485491c3469d3';
 
 // History container
 let searchHistory = [];
+
+function fetchLocation(search) {
+    var url = `${apiRoot}/geo/1.0/direct?q=${search}&limit=5&appid=${apiKey}`;
+
+    fetch(url).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        if(data[0]) {
+            console.log(data[0]);
+            getWeather(data[0]);
+        }
+    }).catch( function (error) {
+        console.log(error);
+    })
+}
+
+function getWeather(location) {
+
+}
